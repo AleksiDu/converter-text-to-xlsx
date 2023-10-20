f6b360cea86d8154ff7fb0038b096de93fbf2556
AleksiDu <aleksi.duluzauri@gmail.com>
2023-10-20 15:23:09 +0400

    create small app for text to xlsx

--- app.ts
+++ app.ts
@@ -5,12 +5,13 @@ const textData = fs.readFileSync("report/runtime.txt", "utf8");
 
 const header = textData.split("|").splice(1, 4);
 
-console.log(header);
-
-console.log(textData.split("\n"));
+const rows = textData
+  .split("\n")
+  .slice(2, textData.length - 1)
+  .map((row) => row.split("|").filter((val) => val !== ""));
 
 const workbook = XLSX.utils.book_new();
-const sheetData = [[...header], ["Data 1", "Data 2", "Data3"]];
+const sheetData = [[...header], ...rows];
 
 const worksheet = XLSX.utils.aoa_to_sheet(sheetData);
 XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
